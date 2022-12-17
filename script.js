b1df19a5c47722585e3db32a2be0f5b214ef0e03
Narcisssss <108619632+Narcisssss@users.noreply.github.com>
2022-12-17 21:03:29 +0200

    Updated files.

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const hamburgerButton = document.getElementById('hamburger');
 const navList = document.getElementById('nav-list');
+const aboutMeImage = document.querySelector('.headshot');
+const information = document.querySelector('.information');
 
 const toggleButton = () => {
     navList.classList.toggle('show');
 }
 
 hamburgerButton.addEventListener('click', toggleButton);
+
+aboutMeImage.addEventListener('click', (event) => {
+    aboutMeImage.classList.toggle('headshot-transition');
+});
+
+window.addEventListener('scroll', () => {
+    let windowHeight = window.innerHeight;
+    let revealTop = information.getBoundingClientRect().top;
+    console.log(information.getBoundingClientRect().top)
+    const revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+        information.classList.add('reveal');
+    } else {
+        information.classList.remove('reveal');
+    }
+})
